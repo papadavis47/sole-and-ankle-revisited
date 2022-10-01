9acbf27e29a9cd9a2832b5e5ec80e4f32fd5685f
John William Davis <jwdavisdeveloper@gmail.com>
2022-10-01 08:47:06 -0700

    Finish Exercise 5 - fluid gap on desktop nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -58,7 +58,7 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
+  overflow: auto;
   border-bottom: 1px solid ${COLORS.gray[300]};
 
   @media ${QUERIES.tabletAndSmaller} {
@@ -75,7 +75,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {
